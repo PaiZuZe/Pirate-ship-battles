fa3049da6f841688d3e5e4091d949719f0721b35
PaiZuZe <vitinho_cga@hotmail.com>
2019-03-17 15:36:42 -0300

    Fixes issues with postgres.

--- create_table.sql
+++ create_table.sql
@@ -1,6 +1,9 @@
+CREATE EXTENSION pgcrypto;
+
 CREATE TABLE players (
     id serial NOT NULL,
     name VARCHAR(20) NOT NULL,
-    highscore bigint DEAFAULT 0,
+    highscore bigint DEFAULT 0,
+    password TEXT NOT NULL,
     PRIMARY KEY (id)
 );
