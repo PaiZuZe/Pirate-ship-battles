f5d5472fda4822a2ef9201083c7728f9570c4ce5
guilhermevieira <xgvieira@gmail.com>
2019-04-24 14:24:16 -0300

    Make simple server app

--- app.ts
+++ app.ts
@@ -2,21 +2,7 @@
 //                            Space  Ship Battles                             //
 ////////////////////////////////////////////////////////////////////////////////
 
-import * as express from "express";
-import * as http from "http";
+import { AppServer } from './server/appServer';
 
-let app = express();
-let serv = new http.Server(app);
-
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-app.use('/client', express.static(__dirname + '/client'));
-
-serv.listen({
-  host: '0.0.0.0',
-  port: 2000,
-  exclusive: true
-});
-
-console.log("Server started succesfully");
+let app = new AppServer().getApp();
+export { app };
---
+++ server/appServer.ts
@@ -0,0 +1,65 @@
+////////////////////////////////////////////////////////////////////////////////
+//                            Space  Ship Battles                             //
+//                                                                            //
+//                             Server - AppServer                             //
+////////////////////////////////////////////////////////////////////////////////
+
+import { createServer, Server } from 'http';
+import * as express from 'express';
+import * as socketIO from 'socket.io';
+
+export class AppServer {
+  private app: express.Application; 
+  private server: Server;
+  private io: socketIO.Server;
+  private readonly port: number = 2000;
+
+  constructor() {
+    this.createApp();
+    this.createServer(); //let serv = new http.Server(app);
+    this.sockets(); //let io = require('socket.io')(serv,{});
+    this.listen();   
+  }
+
+  private createApp(): void {
+    this.app = express();
+  }
+
+  private createServer(): void {
+    this.server = createServer(this.app);
+  }
+
+  private sockets(): void {
+    this.io = socketIO(this.server);
+  }
+
+  private listen(): void {
+    this.server.listen({
+      host: '0.0.0.0',
+      port: this.port,
+      exclusive: true
+      }, () => {
+        console.log('Running server on port %s', this.port)}
+      );
+
+    this.io.on('connect', (socket: any) => {
+      socket.join('login');
+      //socket.on('enter_name', onEntername);
+      //socket.on('logged_in', function(data) {
+        //this.emit('enter_game', {username: data.username});
+        //socket.leave('login');
+        //socket.join('game');
+      //});
+      //socket.on("new_player", onNewPlayer);
+      //socket.on("input_fired", onInputFired);
+      
+      socket.on('disconnect', () => {   //socket.on('disconnect', onClientDisconnect);
+        console.log('Client disconnected');
+      });
+    });
+  }
+  
+  public getApp(): express.Application {
+    return this.app;
+  }
+}
