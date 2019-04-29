b6fa10bfc707571cbd91b1d28f545323696f32e3
guilhermevieira <xgvieira@gmail.com>
2019-04-29 15:22:08 -0300

    Add onEnterName

--- server/appServer.ts
+++ server/appServer.ts
@@ -5,6 +5,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 import { createServer, Server } from 'http';
+import { Pool, Client } from 'pg';
 import * as express from 'express';
 import * as socketIO from 'socket.io';
 import * as path from 'path';
@@ -53,12 +54,12 @@ export class AppServer {
 
     this.io.on('connect', (socket: any) => {
       socket.join('login');
-      //socket.on('enter_name', onEntername);
-      //socket.on('logged_in', function(data) {
-        //this.emit('enter_game', {username: data.username});
-        //socket.leave('login');
-        //socket.join('game');
-      //});
+      socket.on('enter_name', this.onEntername);
+      socket.on('logged_in', function(data) {
+        this.emit('enter_game', {username: data.username});
+        socket.leave('login');
+        socket.join('game');
+      });
       //socket.on("new_player", onNewPlayer);
       //socket.on("input_fired", onInputFired);
       
@@ -66,6 +67,42 @@ export class AppServer {
         console.log('Client disconnected');
       });
     });
+  }
+
+  private createPool(): Pool {
+    const pool: Pool = new Pool({
+      user: 'postgres',
+      host: 'postgres',
+      database: 'ssb',
+      password: 'postgress',
+      port: 5432,
+    })
+    return pool;
+  }
+
+  ////////////////////////////////////////////////////////////////////////////////
+  // Called after the player entered its name
+  private onEntername (data): void {
+    console.log(`Received joinning request from ${this.id}, size: ${data.config.width}:${data.config.height}`);
+    if (data.username.length > 0 && data.username.length < 15) {
+      let pool = this.createPool();
+      pool.query('INSERT INTO players(name, password) VALUES($1, $2)', [data.username, data.password])
+        .then((res) => this.emit('join_game', {username: data.username, id: this.id, password: data.password}))
+        .catch(err => {
+          pool.query('SELECT password FROM players WHERE name = $1', [data.username])
+            .then((res) => {
+              if (res.rows[0].password == data.password)
+                this.emit('join_game', {username: data.username, id: this.id, password: data.password});
+              else this.emit('throw_error', {message: "Player already exists or wrong password"});
+            });
+        })
+        //.catch(err => this.emit('throw_error', {message: "Player already exists or wrong password"}))
+        .finally(() => pool.end());
+    }
+    else if (data.username.length <= 0)
+      this.emit('throw_error', {message: "Name can't be null"});
+    else if (data.username.length >= 15)
+      this.emit('throw_error', {message: "Name is too long"});
   }
   
   public getApp(): express.Application {
