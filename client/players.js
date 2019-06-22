4adfa508ce293d28b9f0b4746124380ba9e137ba
GuilhermeVieira <xgvieira@gmail.com>
2019-06-22 13:42:59 -0300

    Make collision shape follow players

--- client/players.js
+++ client/players.js
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//                            Pirate Ship Battles                             //
+//                             Space Ship Battles                             //
 //                                                                            //
 //                              Client - Players                              //
 ////////////////////////////////////////////////////////////////////////////////
@@ -15,39 +15,35 @@ class Ship {
   constructor (scene, x, y, polygonPoints) {
     // If debug mode active
     // Todo: Make generic function in util.js
-    let colpoly = scene.add.graphics();
-    let color = 0xff0000;
-    let thickness = 4;
+    let color = 0xff0000
     let alpha = 0.5;
-    let smoothness = 64;
-    colpoly.lineStyle(thickness, color, alpha);
-    let poly = new Phaser.Geom.Polygon(polygonPoints.map(point => [x + point[0], y + point[1]]));
-    colpoly.strokePoints(poly.points, true);
-    colpoly.fillStyle(color, alpha);
-    colpoly.fillPoints(poly.points, true);
+    this.colpoly = scene.add.polygon(x, y, polygonPoints, color, alpha); 
+    this.colpoly.setOrigin(0, 0);
+    this.colpoly.setDepth(100000);    
   }
 
-  //////////////////////////////////////////////////////////////////////////////
   update (data) {
-    this.body.x = data.x;
-    this.body.y = data.y;
+    let angle = data.angle * 180 / Math.PI;
+    // Update player collision shape. (DEBUG ONLY)
+    this.colpoly.setPosition(data.x, data.y);
+    this.colpoly.setAngle(angle);
+    // Update player sprite
+    this.body.setPosition(data.x, data.y);
     this.body.setVelocity(Math.sin(data.angle) * data.speed, -(Math.cos(data.angle) * data.speed));
-    this.body.angle = data.angle * 180 / Math.PI;
+    this.body.setAngle(angle);
     this.body.setDepth(data.y);
     this.text.setDepth(data.y);
   }
 
-  //////////////////////////////////////////////////////////////////////////////
   updatePredictive (delta) {
-    this.text.x = this.body.x;
-    this.text.y = this.body.y - LABEL_DIFF;
+    this.text.setPosition(this.body.x, this.body.y - LABEL_DIFF);
     this.text.setDepth(this.body.y);
   }
 
-  //////////////////////////////////////////////////////////////////////////////
   destroy() {
     this.body.destroy();
     this.text.destroy();
+    this.colpoly.destroy();
   }
 }
 
@@ -67,14 +63,12 @@ class Player extends Ship {
         sprite = "ship-alt"
     }
     this.body = scene.physics.add.sprite(x, y, sprite, 0);
-    //this.collisionShape = scene.physics.add.graphics();
     this.text.setOrigin(0.5);
     this.body.setOrigin(0.5);
     this.body.setCircle(1, 16, 32);
     scene.cameras.main.startFollow(this.body);
   }
 
-  //////////////////////////////////////////////////////////////////////////////
   update (data) {
     super.update(data);
     this.life = data.life;
@@ -83,7 +77,6 @@ class Player extends Ship {
   }
 };
 
-////////////////////////////////////////////////////////////////////////////////
 function createPlayer (data) {
   if (!player) {
     player = new Player(this, data.x, data.y, data.username, data.shipname, data.polygonPoints);
@@ -91,12 +84,10 @@ function createPlayer (data) {
   }
 }
 
-////////////////////////////////////////////////////////////////////////////////
 function onPlayerHit (data) {
   let playerExplosion = new Explosion(this, data.x, data.y, 0.8, 30, 380);
 }
 
-////////////////////////////////////////////////////////////////////////////////
 function onRemovePlayer (data) {
 	if (data.id in enemies) {
     let playerExplosion = new Explosion(this, data.x, data.y, 1.2, 50, 450);
@@ -132,12 +123,9 @@ class Enemy extends Ship {
   }
 };
 
-////////////////////////////////////////////////////////////////////////////////
 function createEnemy (data) {
   if (!(data.id in enemies))
     enemies[data.id] = new Enemy(this, data.id, data.x, data.y, data.username, data.polygonPoints);
   else
     console.log("Failed to create enemy");
 }
-
-////////////////////////////////////////////////////////////////////////////////
